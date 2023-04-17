26026432383cf17537e3b27af8ffadecd1c60c0a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 20:35:20 -0300

    style: EmployeeLogin

---
+++ app/src/page/EmployeeLogin/EmployeeLogin.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro';
+
+export const Section = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Logo = styled.img`
+  width: 20rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  gap: 1rem;
+`;
