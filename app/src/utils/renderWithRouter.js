320d4abca0f2cc5f373b363c47687087a57de423
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 20:33:43 -0300

    chore: renderWithRouter

---
+++ app/src/utils/renderWithRouter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component, route = '/') => {
+  const history = createBrowserHistory();
+  history.push(route);
+
+  const { ...resources } = render(
+    <Router history={history}>{component}</Router>
+  );
+  return { ...resources, history };
+};
+
+export default renderWithRouter;
