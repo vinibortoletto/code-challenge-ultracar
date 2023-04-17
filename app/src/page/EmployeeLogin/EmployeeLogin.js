166ed5cf8903a553b862b479fa6dec166db3c71e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 20:35:11 -0300

    feat: EmployeeLogin

--- app/src/page/EmployeeLogin/EmployeeLogin.js
+++ app/src/page/EmployeeLogin/EmployeeLogin.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import fullLogo from '../../assets/full-logo.png';
+import { EmployeeContext } from '../../contexts/EmployeeContext';
+import * as S from './EmployeeLogin.styles';
+import { Button, TextField } from '../../components';
 
 const EmployeeLogin = () => {
-  return <div>EmployeeLogin</div>;
+  const history = useHistory();
+  const { employeeName, setEmployeeName } = useContext(EmployeeContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    history.push('/employee/area');
+  };
+
+  const handleInputChange = (event) => {
+    setEmployeeName(event.target.value);
+  };
+
+  return (
+    <S.Section>
+      <S.Logo src={fullLogo} alt="ultracar-logo" />
+
+      <S.Form onSubmit={handleSubmit}>
+        <TextField
+          label="Nome do funcionário"
+          type="text"
+          id="employee-name"
+          placeholder="Nome do funcionário"
+          value={employeeName}
+          callback={handleInputChange}
+        />
+
+        <Button type="submit" disabled={employeeName === ''}>
+          Entrar
+        </Button>
+      </S.Form>
+    </S.Section>
+  );
 };
 
 export default EmployeeLogin;
