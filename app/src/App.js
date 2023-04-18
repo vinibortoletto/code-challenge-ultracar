dc7b2b6629daa493524cdbe0a71b715cdd0deda1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 22:41:37 -0300

    refactor: context barrel import

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { Header } from './components/';
 import 'antd/dist/reset.css';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { useLocation } from 'react-router-dom';
-import { EmployeeProvider } from './contexts/EmployeeContext';
+import { EmployeeProvider } from './contexts';
 
 function App() {
   const location = useLocation();
---
+++ app/src/contexts/index.js
@@ -0,0 +1,1 @@
+export { EmployeeContext, EmployeeProvider } from './EmployeeContext';
