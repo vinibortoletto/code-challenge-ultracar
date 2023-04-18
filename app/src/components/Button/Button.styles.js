9b4c59683a576f70b192b314a68d8880e9a4f177
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 22:41:51 -0300

    style: button padding

--- app/src/components/Button/Button.styles.js
+++ app/src/components/Button/Button.styles.js
@@ -4,9 +4,8 @@ import { vars } from '../../styles/variables';
 const Button = styled.button`
   background-color: ${vars.color.teal};
   color: ${vars.color.white};
-  padding: ${vars.padding.default};
+  padding: ${vars.padding.default} 1rem;
   border-radius: ${vars.radius.default};
-  width: 6rem;
 
   background-color: ${({ disabled }) => disabled && vars.color.grey};
 `;
