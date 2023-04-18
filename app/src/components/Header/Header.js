fa4593c3af6447d3498b4653834c3bbbd091dca3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-18 16:29:19 -0300

    fix: clear employee from localStorage after logout

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import simpleLogo from '../../assets/simple-logo.png';
 import * as S from './Header.styles';
 import { useHistory } from 'react-router-dom';
+import { EmployeeContext } from '../../contexts';
 
 const Header = () => {
   const history = useHistory();
+  const { setEmployeeName } = useContext(EmployeeContext);
 
   const handleLogout = () => {
+    localStorage.setItem('employee', JSON.stringify(''));
+    setEmployeeName('');
     history.push('/employee/login');
   };
 
