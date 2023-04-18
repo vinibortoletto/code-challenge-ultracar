44d9483dfb1417635ddda41f8a91fdcc40134ed7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-18 14:12:23 -0300

    feat: ClientForm page

---
+++ app/src/page/NewService/ClientForm/ClientForm.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/macro';
+
+export const Section = styled.section`
+  button {
+    width: 10rem;
+    margin: auto;
+  }
+`;
+
+export const PersonalFields = styled.div`
+  grid-template-columns: 3fr 1fr;
+`;
+
+export const AddressFields = styled.div`
+  grid-template-columns: 2fr 3fr 1fr;
+`;
+
+export const CarFields = styled.div`
+  grid-template-columns: 2fr 1fr 1fr;
+`;
