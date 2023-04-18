e2b6da7c6477ba08f78f9cb64b6c0c5299182870
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-18 00:13:39 -0300

    style: remove unused global styles

--- app/src/styles/GlobalStyles.js
+++ app/src/styles/GlobalStyles.js
@@ -9,7 +9,6 @@ export const GlobalStyles = createGlobalStyle`
     font-family: 'Open Sans', sans-serif;
     background-color: white;
     color: ${vars.color.black};
-    
   } 
 
   .section_margin {
@@ -18,39 +17,5 @@ export const GlobalStyles = createGlobalStyle`
   
   .content {
     padding: 0 var(--m_sm);
-  }
-
-  input, select {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-  }
-
-  input::-webkit-input-placeholder {
-    color: var(--d_green);
-    opacity: 0.5;
-  }
-  input::-moz-placeholder {
-    color: var(--d_green);
-    opacity: 0.5;
-  }
-  textarea::-webkit-input-placeholder {
-    color: var(--d_green);
-    opacity: 0.5;
-  }
-  textarea::-moz-placeholder {
-    color: var(--d_green);
-    opacity: 0.5;
-  }
- 
-  label[for=search_products] {
-    border: 0;
-    clip: rect(0 0 0 0);
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    width: 1px;
   }
 `;
