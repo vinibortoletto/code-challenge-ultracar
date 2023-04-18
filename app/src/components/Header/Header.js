38ddfbac4b41d16b022ec184210edbdf674d1e47
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-17 21:58:02 -0300

    feat: header logout button

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import simpleLogo from '../../assets/simple-logo.png';
 import * as S from './Header.styles';
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.push('/employee/login');
+  };
+
   return (
     <S.Header>
       <S.Logo src={simpleLogo} alt="ultracar-logo" />
-      <S.Button type="button">Sair</S.Button>
+      <S.Button type="button" onClick={handleLogout}>
+        Sair
+      </S.Button>
     </S.Header>
   );
 };
