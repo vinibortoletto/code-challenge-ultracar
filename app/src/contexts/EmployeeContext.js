50ce0a5f0e604763e71e77c9cbe59e3ca80c4b13
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-18 00:14:40 -0300

    refactor: remove unnecessary hooks

--- app/src/contexts/EmployeeContext.js
+++ app/src/contexts/EmployeeContext.js
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { node } from 'prop-types';
 
 export const EmployeeContext = createContext();
@@ -12,23 +6,20 @@ export const EmployeeContext = createContext();
 export function EmployeeProvider({ children }) {
   const [employeeName, setEmployeeName] = useState('');
 
-  const getEmployeeFromLocalStorage = useCallback(() => {
+  const getEmployeeFromLocalStorage = () => {
     const localEmployee = localStorage.getItem('employee');
     if (!localEmployee) return localStorage.setItem('employee', employeeName);
     setEmployeeName(localEmployee);
-  }, []);
+  };
 
   useEffect(() => {
     getEmployeeFromLocalStorage();
   }, []);
 
-  const value = useMemo(
-    () => ({
-      employeeName,
-      setEmployeeName,
-    }),
-    [employeeName, setEmployeeName]
-  );
+  const value = {
+    employeeName,
+    setEmployeeName,
+  };
 
   return (
     <EmployeeContext.Provider value={value}>
