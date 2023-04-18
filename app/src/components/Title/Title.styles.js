83c179c0758c80e7fae1da48bdf94851544c6c4b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-18 16:44:01 -0300

    style: responsive Title

--- app/src/components/Title/Title.styles.js
+++ app/src/components/Title/Title.styles.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components/macro';
 import { vars } from '../../styles/variables';
+import { breakpoint } from '../../styles/breakpoint';
 
 export const Container = styled.div`
-  font-size: 2rem;
   font-weight: bold;
   color: ${vars.color.teal};
+  font-size: 1.5rem;
   margin: 1rem 0;
+
+  @media ${breakpoint('600')} {
+    font-size: 2rem;
+  }
 `;
